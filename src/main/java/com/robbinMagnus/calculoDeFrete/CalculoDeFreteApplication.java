1d765c30fa05f9fc5166d690bdcf1a77d1982fc4
RobinMagnus <robbin333@outlook.com.br>
2025-04-12 16:54:50 -0300

    Primeiro commit criando repositorio no git

---
+++ src/main/java/com/robbinMagnus/calculoDeFrete/CalculoDeFreteApplication.java
@@ -0,0 +1,13 @@
+package com.robbinMagnus.calculoDeFrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculoDeFreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculoDeFreteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robbinMagnus/calculoDeFrete/CalculoDeFreteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robbinMagnus.calculoDeFrete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculoDeFreteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
